0c1f104bed812bf0bc5c6a1f356128d7bc9a5130
myanksinghal <vohawan1999@gmail.com>
2023-07-06 12:23:10 +0200

    test params changed

--- consts.h
+++ consts.h
@@ -7,10 +7,10 @@ const float G = 1.0;
 //G=1, using Dist=1pc, total mass M = 1 Msun then V=0.06558km/s
 
 const double THETA_THRESHOLD = 0.4;
-const double SOFTENING_PARAM = 0.1;
+const double SOFTENING_PARAM = 0.05;
 const float alpha=-2.35; //Power for powerlaw profile
 const int NODE_CAPACITY = 1;
-const double neta = 0.01;
+const double neta = 0.001;
 const double max_timestep = 0.01;
 const double OUT_TIMESTEP =0.001;
 extern bool increase_nstep;
@@ -22,7 +22,7 @@ extern unsigned int n_time_blocks;
 extern double t1;
 
 const bool external_body=true;
-const float ext_mass = 1.0;
+const float ext_mass = 1.5;
 const float ext_halfmass_rad=5.0;
 const std::vector<double> ext_r={0,0,0};
 const std::vector<double> ext_v={0,0,0};
--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
 	//infile = fopen("n6.pos", "r");
 	//nbd_sys temp_sys(infile);
 	//fclose(infile);
-	nbd_sys temp_sys(1000, 0.2, 150.0, 2.0,0.27);
+	nbd_sys temp_sys(1000, 0.2, 150.0, 1.0,0.4);
 
 	vector<double> cent = {0.0, 0.0, 0.0};
 	FILE *outfile;
